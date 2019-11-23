82402ffd50a42a9463f0216534a93fc3b7de392c
Monica Scroggs <scrmr0210@gmail.com>
2019-11-23 11:55:44 -0500

    push input to firebase, firebase working

--- javascript/train.js
+++ javascript/train.js
@@ -13,14 +13,72 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
+// Initial Values
 var trainName = "";
 var destination = "";
-var firstTime = "";
 var frequency = 0;
+var nextArrival = "";
+
+// Set Current Time
+function setTime() {
+    var current = moment().format('LTS');
+    $(".currentTime").html(current);
+    setTimeout(setTime, 1000);
+};
+
+setTime();
+
+
+// Capture Button Click
+$("#submitBtn").on("click", function (event) {
+    event.preventDefault();
+
+    console.log("button working");
+
+    // Grabbed values from text-boxes
+    trainName = $("#train-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    nextArrival = $("#arrival-input").val().trim();
+
+    // Code for "Setting values in the database"
+    database.ref().set({
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        nextArrival: nextArrival,
+    });
+
+});
+
+// Firebase watcher + initial loader HINT: .on("value")
+database.ref().on("value", function (snapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().frequency);
+    console.log(snapshot.val().nextArrival);
+
+});
+
+//   // Change the HTML to reflect
+//   $("#name-display").text(snapshot.val().name);
+//   $("#email-display").text(snapshot.val().email);
+//   $("#age-display").text(snapshot.val().age);
+//   $("#comment-display").text(snapshot.val().comment);
+
+//   // Handle the errors
+// }, function(errorObject) {
+//   console.log("Errors handled: " + errorObject.code);
+// });
+
+
 
 
 // add new row with train info every time click submit
-$(".btn btn-primary").on('click', function (event) {
+$("#submitBtn").on('click', function (event) {
     event.preventDefault();
 
 
